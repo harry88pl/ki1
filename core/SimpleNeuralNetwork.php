ad9b7df05c834027b5b4e127f840c77a99151a36
Charytoniuk <adrian.charytoniuk@maxpool.de>
2023-07-18 18:27:57 +0200

    it work.

---
+++ core/SimpleNeuralNetwork.php
@@ -0,0 +1,51 @@
+<?php
+class SimpleNeuralNetwork {
+
+    public $weights;
+
+    public function __construct() {
+        // Seed the random number generator.
+        srand(1);
+
+        // Initialize the weights to random values.
+        /*$this->weights = array(
+            array(2 * rand(0, 1) - 1),
+            array(2 * rand(0, 1) - 1),
+            array(2 * rand(0, 1) - 1),
+        );*/
+        $this->weights = [
+            [-0.16595599],
+            [ 0.44064899],
+            [-0.99977125]];
+    }
+
+    public function train($train_input, $train_output, $train_iters) {
+        for ($i = 0; $i < $train_iters; $i++) {
+            $propagation_result = $this->propagation($train_input);
+            $this->backward_propagation($propagation_result, $train_input, $train_output);
+        }
+    }
+
+    function backward_propagation($propagation_result, $train_input, $train_output) {
+        $error = matrix::subtract($train_output, $propagation_result);
+        $d_sigmoid = d_sigmoid($propagation_result);
+        $multiply = matrix::multiplyAdrian($error, $d_sigmoid);
+        $this->weights = matrix::sum($this->weights, matrix::dot(matrix::transpose($train_input), $multiply));
+    }
+
+    public function propagation($inputs, $debug = false) {
+        if ($debug) {
+            var_dump('$inputs', $inputs);
+            var_dump('$this->weights', $this->weights);
+        }
+        $dot = matrix::dot($inputs, $this->weights);
+        if ($debug) {
+            var_dump('$dot', $dot);
+        }
+        $sigmoid = sigmoid($dot);
+        if ($debug) {
+            var_dump('$sigmoid', $sigmoid);
+        }
+        return $sigmoid;
+    }
+}
--- core/functions.php
+++ core/functions.php
@@ -1,30 +1,10 @@
 <?php
-function dot($matrix1, $matrix2) {
-    var_Dump($matrix1);
-    var_Dump($matrix2);
-    $result = array();
-    $rows1 = sizeof($matrix1);
-    $cols1 = sizeof($matrix1[0]);
-    $rows2 = sizeof($matrix2);
-    $cols2 = sizeof($matrix2[0]);
 
-    if ($cols1 != $rows2) {
-        throw new Exception("Invalid matrix dimensions for dot product");
-    }
-
-    for ($i = 0; $i < $rows1; $i++) {
-        $row = array();
-        for ($j = 0; $j < $cols2; $j++) {
-            $sum = 0;
-            for ($k = 0; $k < $cols1; $k++) {
-                $sum += $matrix1[$i][$k] * $matrix2[$k][$j];
-            }
-            $row[] = $sum;
-        }
-        $result[] = $row;
-    }
-
-    return $result;
+function sigmoid($matrix, $const = 1) {
+    return matrix::divide_constant($const, (matrix::add_constant($const, matrix::exp(matrix::reverse($matrix)))));
 }
-
+function d_sigmoid($matrix, $const = 1) {
+    $sub = matrix::subtract_constant($const, $matrix);
+    return matrix::multiplyAdrian($matrix, $sub);
+}
 
